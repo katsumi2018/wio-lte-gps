673c24bfcbc53a271f9c2bd70c3a6762982c79ed
katsumi2018 <k-maki@wh.commufa.jp>
2019-06-14 23:53:29 +0900

    Update wio-lte-gps.ino

--- wio-lte-gps.ino
+++ wio-lte-gps.ino
@@ -3,19 +3,22 @@
 #define BUTTON_PIN  (WIOLTE_D38)
 
 WioLTE Wio;
-#define INTERVAL    (100)
-
-int buttonState_before = 0 ;
-int buttonState_now = 0 ;
 
 // ----------------------
 // 初期値一覧
 // ----------------------
 
+#define INTERVAL    (2000)  // LOOP定期実行時間(msec)
+int regular_action_interval_time_min = 5; // 定期的にSORACOMへ送信する間隔(分)
+float map_difference_threshold = 0.0001;  // 緯度 or 経度の差でデータ送信する閾値
+                                          // 0.001 でだいたい80m~110mくらい
+
 int gps_interval_time_sec = 10; // GPS取得に諦める時間(秒)
-int regular_action_interval_time_min = 5; // 定期的にGPS取得する間隔(分)
 
 // ----------------------
+
+int buttonState_before = 0 ;
+int buttonState_now = 0 ;
 
 int gps_interval_time_msec = gps_interval_time_sec * 1000 ;
 int regular_action_interval_time_msec = regular_action_interval_time_min * 1000 * 60 ;
@@ -23,34 +26,92 @@ int regular_action_interval_time_msec = regular_action_interval_time_min * 1000 
 float latitude_before = 0;
 float longitude_before = 0;
 
+char* gps_quality = "";
+const char* gps_data;
+String str_gps_data = "";
+char gps_data_list[100];
+  
+String latitude_str = "";
+String longitude_str = "";
+String send_gps_data = "" ;
+char send_gps_char[80];
+  
+float map_float;
+int map_int;
+float map_few;
+float map_difference = 0;
+int map_difference_check;
+
 int start_time;
 
-void send_to_soracom()
+
+void setup()
 {
+  delay(200);
 
-  // -------------------------
-  // GET GPS DATA
-  // -------------------------
-  const char* gps_data;
-  String str_gps_data = "";
+  SerialUSB.println("");
+  SerialUSB.println("--- START ---------------------------------------------------");
   
+  SerialUSB.println("### I/O Initialize.");
+  GpsBegin(&Serial);
+  Wio.Init();
+
+  SerialUSB.println("### Power supply ON.");
+  Wio.PowerSupplyGrove(true);
+  delay(500);
+
+  pinMode(BUTTON_PIN, INPUT);
+
+  start_time = millis();
+  
+  SerialUSB.println("### Setup completed.");
+  
+}
+
+void loop()
+{
+  // ----------------------
+  // GPSの情報を取得するアクション
+  // ----------------------
+  get_gps_data();
+  SerialUSB.println(send_gps_char);
+  
+  // ----------------------
+  // ボタンでのアクション
+  // ----------------------
+  int buttonState_now = digitalRead(BUTTON_PIN);
+  // ボタンONの時
+  if ((buttonState_now == 1) && (buttonState_before == 0)){
+    SerialUSB.println("BUTTON PUSH");
+    send_to_soracom();
+    buttonState_before = 1;
+  // ボタンOFFの時
+  } else if ((buttonState_now == 0) && (buttonState_before == 1)){
+    buttonState_before = 0;
+  } 
+  
+  // ----------------------
+  // 定期的にSORACOMへデータを送るアクション
+  // ----------------------
+  int now_time = millis();
+  
+  int gps_progress_time = now_time - start_time;
+  if (gps_progress_time > regular_action_interval_time_msec) {
+    start_time = millis();
+    send_to_soracom();
+  }
+
+  delay(INTERVAL);
+  
+}
+
+// -------------------------
+// GET GPS DATA
+// -------------------------
+void get_gps_data() {
+
   char* tmp = "";
-  char* gps_quality = "";
-
-  char gps_data_list[100];
-  
-  String latitude_str = "";
-  String longitude_str = "";
-  String send_gps_data = "" ;
-  char send_gps_char[80];
-  
-  float map_float;
-  int map_int;
-  float map_few;
-  float map_difference = 0;
-  int map_difference_check = 0;
-
-  
+ 
   // 取得開始の時間を取得
   int gps_start_time = millis();
   int gps_now_time = millis();
@@ -99,7 +160,7 @@ void send_to_soracom()
         map_float = map_int + ((map_few / 60) * 100);
         // 前回との差を確認する
         map_difference = map_float - longitude_before;
-        if ((map_difference > 0.001)||(map_difference < -0.001)) {
+        if ((map_difference > map_difference_threshold)||(map_difference < (map_difference_threshold * -1))) {
           map_difference_check = 1;
         }
         longitude_before = map_float ;   // 次回との比較用に今回の値と保存
@@ -114,7 +175,10 @@ void send_to_soracom()
         // string を char に変換
         send_gps_data.toCharArray(send_gps_char, 80) ;
 
-        SerialUSB.println("GPS-DATA GET");
+        SerialUSB.print("GPS-DATA GET(");
+        SerialUSB.print(map_difference_check);
+        SerialUSB.print(")");
+        
         //SerialUSB.println(gps_data);
         
         break;
@@ -125,18 +189,18 @@ void send_to_soracom()
     gps_now_time = millis();
     int gps_progress_time = gps_now_time - gps_start_time;
     if (gps_progress_time > gps_interval_time_msec) {
-      SerialUSB.println("GPS-GET TIMEOUT");
+      SerialUSB.print("GPS-GET TIMEOUT : ");
       break;
     }
-    
   }
+ 
+}
 
-
-  // -------------------------
-  // SORACOOM SEND GPS DATA
-  // -------------------------
-
-
+// -------------------------
+// SORACOOM SEND GPS DATA
+// -------------------------
+void send_to_soracom()
+{
   if ((strncmp(gps_quality,"1",1) == 0)&&(map_difference_check == 1)) {
     Wio.PowerSupplyLTE(true);
     
@@ -169,6 +233,7 @@ void send_to_soracom()
     }
  
     SerialUSB.println("SocketSend");
+    map_difference_check = 0;
     char res[128];
     int len;
     len = Wio.SocketReceive(connectId, res, sizeof (res), 3000);
@@ -197,62 +262,6 @@ void send_to_soracom()
     Wio.PowerSupplyLTE(false);
     gps_quality = "" ;
   }
-  
-}
-
-void setup()
-{
-  delay(200);
-
-  SerialUSB.println("");
-  SerialUSB.println("--- START ---------------------------------------------------");
-  
-  SerialUSB.println("### I/O Initialize.");
-  GpsBegin(&Serial);
-  Wio.Init();
-
-  SerialUSB.println("### Power supply ON.");
-  Wio.PowerSupplyGrove(true);
-  delay(500);
-
-  pinMode(BUTTON_PIN, INPUT);
-
-  start_time = millis();
-  
-  SerialUSB.println("### Setup completed.");
-  
-  
-}
-
-
-
-void loop()
-{
-
-  // ボタンでのアクション
-  int buttonState_now = digitalRead(BUTTON_PIN);
-  // ボタンONの時
-  if ((buttonState_now == 1) && (buttonState_before == 0)){
-    send_to_soracom();
-    buttonState_before = 1;
-  }
-  // ボタンOFFの時
-  if ((buttonState_now == 0) && (buttonState_before == 1)){
-    buttonState_before = 0;
-  }
-
-
-  // 定期実行のアクション
-
-  int now_time = millis();
-  
-  int gps_progress_time = now_time - start_time;
-  if (gps_progress_time > regular_action_interval_time_msec) {
-    start_time = millis();
-    send_to_soracom();
-  }
-
-  delay(INTERVAL);
   
 }
 
